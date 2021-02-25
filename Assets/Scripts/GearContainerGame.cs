fc2a585620a93cc8b285f241a161f60974426868
MuriloPs <murilinhorps@gmail.com>
2021-02-25 14:22:59 -0300

    Add GearContainer to be gear parent and rotate it

---
+++ Assets/Scripts/GearContainerGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearContainerGame : MonoBehaviour
+{
+    public bool IsOccupied;
+
+    public int Direction = 1;
+
+    public bool CanRotate;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (transform.childCount == 0)
+        {
+            SetIsOccupied(false);
+        }
+
+        if (CanRotate)
+            RotateGear();
+    }
+
+    void RotateGear()
+    {
+        var gearTransform = transform.GetChild(0).transform;
+
+        gearTransform.RotateAround(transform.position, new Vector3(0, 0, Direction), 180 * Time.deltaTime);
+    }
+
+    public void SetIsOccupied(bool isOccupied) =>
+        this.IsOccupied = isOccupied;
+}
