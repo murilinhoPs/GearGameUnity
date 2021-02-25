f39ecbb919155bff5720a4e001e70073d60f201f
MuriloPs <murilinhorps@gmail.com>
2021-02-25 14:22:10 -0300

    Add script to check for collisions

---
+++ Assets/Scripts/CheckRaycastCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CheckRaycastCollision : MonoBehaviour
+{
+    static public RaycastHit2D CheckCollision(LayerMask layerToDetect)
+    {
+        Ray worldPointRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var distance = worldPointRay.direction.z - worldPointRay.origin.z;
+
+        RaycastHit2D hit = Physics2D.Raycast(worldPointRay.origin, worldPointRay.direction, distance, layerToDetect);
+
+        Debug.DrawRay(worldPointRay.origin, worldPointRay.direction, Color.red);
+
+        // if (hit)
+        // {
+        //     print("hit something: " + hit.transform.name);
+
+        // }
+
+        return hit;
+
+    }
+}
