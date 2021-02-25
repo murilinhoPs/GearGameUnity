7066d2ebc91599287c226ffee0af8a8128367db9
MuriloPs <murilinhorps@gmail.com>
2021-02-25 14:23:12 -0300

    Crete drag handler for game Gear

---
+++ Assets/Scripts/GearDragHandler.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class GearDragHandler : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector2 mousePosition;
+
+    private Vector3 collisionTransform;
+    private RaycastHit2D receivedRaycast;
+
+    public LayerMask layerMask;
+
+    void Start()
+    {
+        mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        transform.position = curPosition;
+
+        receivedRaycast = CheckRaycastCollision.CheckCollision(layerMask);
+    }
+
+    void OnMouseUp()
+    {
+        if (receivedRaycast)
+        {
+            var container = receivedRaycast.transform.GetComponent<GearContainerGame>();
+
+            if (container.IsOccupied)
+            {
+                ResetPosition();
+                return;
+            }
+
+            transform.SetParent(receivedRaycast.transform);
+            ResetPosition();
+
+            container.SetIsOccupied(true);
+        }
+        else
+            ResetPosition();
+
+    }
+
+    void ResetPosition() => transform.localPosition = Vector3.zero;
+}
---
+++ Assets/Scripts/ItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ItemModel
+{
+    public ItemModel(Color color)
+    {
+        this.color = color;
+    }
+
+    public Color color { get; set; }
+}
